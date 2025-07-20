e62476d8fbd74e3ddb945e6962021a5b9d2f1e26
unknown <ryan.leroux77@outlook.com>
2025-07-20 22:11:21 +0000

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+int value = 10;
+
+
+if (true)
+{
+    value = 15;
+    Console.WriteLine($"Value: {value}");
+}
+
+
+
+Console.WriteLine($"Value: {value}");
---
+++ oldd/Program-old-1.cs
@@ -0,0 +1,14 @@
+﻿//string permission = "Admin";
+//int level = 22;
+
+//string greeting = $"{(permission.Contains("Admin") ? (level > 55 ? "Welcome, Super Admin user." : "Welcome, Admin user.") : (permission.Contains("Manager") ? (level > 19 ? "Contact an Admin for access" : "You do not have sufficient privileges") : "You do not have sufficient privileges"))}";
+
+//string adminMsg = $"Welcome, {(level > 55 ? "Super " : "")}Admin user.";
+
+
+//string greetingNonAdmin = $"{(permission.Contains("Manager") ? (level > 19 ? "Contact an Admin for access." : "You do not have sufficient privileges") : "You do not have sufficient privileges")}";
+
+
+
+//Console.WriteLine(greeting);
+
