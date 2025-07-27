aec135897b26a7ab6d7d8ac5afdd7c2d7b16ea68
unknown <ryan.leroux77@outlook.com>
2025-07-27 09:19:39 +0000

    C# switch case act https://learn.microsoft.com/en-us/training/modules/csharp-switch-case/2-exercise-switch-case

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
-Random dice = new Random();
-int roll = dice.Next(1, 7);
-Console.WriteLine(roll);
+int score = 3;
+
+
+switch (score)
+{
+    case 1:
+        Console.WriteLine("Could do better");
+        break;
+
+
+    case 2:
+        Console.WriteLine("Getting there");
+        break;
+
+
+    case 3:
+        Console.WriteLine("Well done!");
+        break;
+}
+
--- obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0df8548bcc9a2df79e3a28c2843680a7395ed2ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
